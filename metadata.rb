86f8c75261588dc44f44a90de9ac9da5de528645
Raul Rodriguez <raul@onddo.com>
2013-07-01 23:10:51 +0200

    empty cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'owncloud'
+maintainer       'Onddo Labs, Sl.'
+maintainer_email 'team@onddo.com'
+license          'Apache 2.0'
+description      'Installs/Configures owncloud'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
