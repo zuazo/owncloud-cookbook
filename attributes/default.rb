0805d555317ab4b0785fa7d1ec849bcb64dc112d
Raul Rodriguez <raul@onddo.com>
2013-07-26 03:45:01 +0200

    compatibility with rhel platform family

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,10 @@
 default['owncloud']['version'] = '5.0.7'
 default['owncloud']['download_url'] = "http://download.owncloud.org/community/owncloud-#{node['owncloud']['version']}.tar.bz2"
 
-default['owncloud']['www_dir'] = '/var/www'
+default['owncloud']['www_dir'] = value_for_platform_family(
+  ['rhel', 'fedora'] => '/var/www/html',
+  'default' => '/var/www',
+)
 default['owncloud']['dir'] = "#{node['owncloud']['www_dir']}/owncloud"
 default['owncloud']['data_dir'] = "#{node['owncloud']['dir']}/data"
 default['owncloud']['server_name'] = node['fqdn']
