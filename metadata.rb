df8ba74c5698e1e16cb273e2c59cc54811154d4f
Raul Rodriguez <raul@onddo.com>
2013-07-28 23:25:28 +0200

    recipe and attribute descriptions added to metadata

--- metadata.rb
+++ metadata.rb
@@ -2,13 +2,193 @@ name             'owncloud'
 maintainer       'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
-description      'Installs/Configures owncloud'
+description      'Installs/Configures ownCloud'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
 
 depends 'apache2'
 depends 'database'
 depends 'mysql'
-depends 'php'
 depends 'openssl'
+depends 'php'
 depends 'postfix'
+
+recipe 'owncloud::default', 'Installs and configures ownCloud'
+
+attribute 'owncloud/version',
+  :display_name => 'ownCloud Version',
+  :description => 'Version of ownCloud to install',
+  :type => 'string',
+  :required => 'optional',
+  :default => '"5.0.7"'
+
+attribute 'owncloud/download_url',
+  :display_name => 'ownCloud Download Url',
+  :description => 'Url from where ownCloud will be downloaded',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'owncloud/www_dir',
+  :display_name => 'ownCloud www Dir',
+  :description => 'Root directory defined in Apache where web documents are stored',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'owncloud/dir',
+  :display_name => 'ownCloud Dir',
+  :description => 'Directory where ownCloud will be installed',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'owncloud/data_dir',
+  :display_name => 'ownCloud Data Dir',
+  :description => 'Directory where ownCloud data will be stored',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'owncloud/server_name',
+  :display_name => 'ownCloud Server Name',
+  :description => 'Sets the server name for the ownCloud virtual host',
+  :calculated => true,
+  :type => 'string',
+  :required => 'recommended'
+
+attribute 'owncloud/ssl',
+  :display_name => 'ownCloud Use SSL?',
+  :description => 'Whether ownCloud should accept requests through SSL',
+  :choice => [ 'true', 'false' ],
+  :type => 'string',
+  :required => 'optional',
+  :default => 'true'
+
+attribute 'owncloud/admin/user',
+  :display_name => 'ownCloud Admin Username',
+  :description => 'Administrator username',
+  :type => 'string',
+  :required => 'optional',
+  :default => '"admin"'
+
+attribute 'owncloud/admin/pass',
+  :display_name => 'ownCloud Admin Password',
+  :description => 'Administrator password',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'owncloud/config/dbtype',
+  :display_name => 'ownCloud Database Type',
+  :description => 'Type of database, only mysql supported for now',
+  :choice => [ '"mysql"' ],
+  :type => 'string',
+  :required => 'optional',
+  :default => '"mysql"'
+
+attribute 'owncloud/config/dbname',
+  :display_name => 'ownCloud Database Name',
+  :description => 'Name of the ownCloud database',
+  :type => 'string',
+  :required => 'optional',
+  :default => '"owncloud"'
+
+attribute 'owncloud/config/dbuser',
+  :display_name => 'ownCloud Database User',
+  :description => 'User to access the ownCloud database',
+  :type => 'string',
+  :required => 'optional',
+  :default => '"owncloud"'
+
+attribute 'owncloud/config/dbpassword',
+  :display_name => 'ownCloud Database Password',
+  :description => 'Password to access the ownCloud database',
+  :calculated => true,
+  :type => 'string',
+  :required => 'optional'
+
+attribute 'owncloud/config/dbhost',
+  :display_name => 'ownCloud Database Host',
+  :description => 'Host running the ownCloud database',
+  :type => 'string',
+  :required => 'optional',
+  :default => '"localhost"'
+
+attribute 'owncloud/config/dbtableprefix',
+  :display_name => 'ownCloud Database Table Prefix',
+  :description => 'Prefix for the ownCloud tables in the database',
+  :type => 'string',
+  :required => 'optional',
+  :default => '""'
+
+attribute 'owncloud/config/mail_smtpmode',
+  :display_name => 'ownCloud Mail SMTP Mode',
+  :description => 'Mode to use for sending mail, can be sendmail, smtp, qmail or php',
+  :choice => [ '"sendmail"', '"smtp"', '"qmail"', '"php"' ],
+  :type => 'string',
+  :required => 'optional',
+  :default => '"sendmail"'
+
+attribute 'owncloud/config/mail_smtphost',
+  :display_name => 'ownCloud Mail SMTP Host',
+  :description => 'Host to use for sending mail, depends on mail_smtpmode if this is used',
+  :type => 'string',
+  :required => 'optional',
+  :default => '"127.0.0.1"'
+
+attribute 'owncloud/config/mail_smtpport',
+  :display_name => 'ownCloud Mail SMTP Port',
+  :description => 'Port to use for sending mail, depends on mail_smtpmode if this is used',
+  :type => 'string',
+  :required => 'optional',
+  :default => '25'
+
+attribute 'owncloud/config/mail_smtptimeout',
+  :display_name => 'ownCloud Mail SMTP Timeout',
+  :description => 'SMTP server timeout in seconds for sending mail, depends on mail_smtpmode if this is used',
+  :type => 'string',
+  :required => 'optional',
+  :default => '10'
+
+attribute 'owncloud/config/mail_smtpsecure',
+  :display_name => 'ownCloud Mail SMTP Secure',
+  :description => 'SMTP connection prefix or sending mail, depends on mail_smtpmode if this is used. Can be "", ssl or tls',
+  :choice => [ '""', '"ssl"', '"tls"' ],
+  :type => 'string',
+  :required => 'optional',
+  :default => '""'
+
+attribute 'owncloud/config/mail_smtpauth',
+  :display_name => 'ownCloud Mail SMTP Auth',
+  :description => 'Whether authentication is needed to send mail, depends on mail_smtpmode if this is used',
+  :choice => [ 'true', 'false' ],
+  :type => 'string',
+  :required => 'optional',
+  :default => 'false'
+
+attribute 'owncloud/config/mail_smtpauthtype',
+  :display_name => 'ownCloud Mail SMTP Auth Type',
+  :description => 'authentication type needed to send mail, depends on mail_smtpmode if this is used. Can be LOGIN, PLAIN or NTLM',
+  :choice => [ '"LOGIN"', '"PLAIN"', '"NTLM"' ],
+  :type => 'string',
+  :required => 'optional',
+  :default => '"LOGIN"'
+
+attribute 'owncloud/config/mail_smtpname',
+  :display_name => 'ownCloud Mail SMTP Name',
+  :description => 'Username to use for sendmail mail, depends on mail_smtpauth if this is used',
+  :type => 'string',
+  :required => 'optional',
+  :default => '""'
+
+attribute 'owncloud/config/mail_smtppassword',
+  :display_name => 'ownCloud Mail SMTP Password',
+  :description => 'Password to use for sendmail mail, depends on mail_smtpauth if this is used',
+  :type => 'string',
+  :required => 'optional',
+  :default => '""'
