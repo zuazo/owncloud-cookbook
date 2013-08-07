2993e558369779e21d1ecb3fd226c6442dc14b26
Raul Rodriguez <raul@onddo.com>
2013-08-07 14:14:55 +0200

    version bumped to 0.1.1 WiP

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
 description      'Installs/Configures ownCloud'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1' # WiP
 
 supports 'centos'
 supports 'debian'
