9355ad3326dd5cd0abc7333d186dad30de59f46e
Xabier de Zuazo <xabier@onddo.com>
2013-09-03 11:21:08 +0200

    PHP-FPM installation moved to its own recipe

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: owncloud
-# Recipe:: apache2
+# Recipe:: nginx
 #
 # Copyright 2013, Onddo Labs, Sl.
 #
@@ -22,13 +22,7 @@
 #==============================================================================
 
 include_recipe 'nginx'
-node.default['php-fpm']['pool']['testpool']['listen'] = '127.0.0.1:9000'
-web_server = node['owncloud']['web_server']
-node.default['php-fpm']['pool']['www']['user'] = node[web_server]['user']
-node.default['php-fpm']['pool']['testpool']['user'] = node[web_server]['user']
-node.default['php-fpm']['pool']['www']['group'] = node[web_server]['group']
-node.default['php-fpm']['pool']['testpool']['group'] = node[web_server]['group']
-include_recipe 'php-fpm'
+include_recipe 'owncloud::php_fpm'
 
 # Disable default site
 nginx_site 'default' do
---
+++ recipes/php_fpm.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: owncloud
+# Recipe:: php_fpm
+#
+# Copyright 2013, Onddo Labs, Sl.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#==============================================================================
+# Installs PHP-FPM
+#==============================================================================
+
+node.default['php-fpm']['pool']['testpool']['listen'] = '127.0.0.1:9000'
+web_server = node['owncloud']['web_server']
+node.default['php-fpm']['pool']['www']['user'] = node[web_server]['user']
+node.default['php-fpm']['pool']['testpool']['user'] = node[web_server]['user']
+node.default['php-fpm']['pool']['www']['group'] = node[web_server]['group']
+node.default['php-fpm']['pool']['testpool']['group'] = node[web_server]['group']
+include_recipe 'php-fpm'
