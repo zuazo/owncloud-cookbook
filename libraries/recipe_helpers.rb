6e91865d51fe0b6f02fe6baf2bf7ae3825efa28c
Raul Rodriguez <raul@onddo.com>
2014-01-22 17:40:17 +0100

    only generate the certificate if needed

--- libraries/recipe_helpers.rb
+++ libraries/recipe_helpers.rb
@@ -3,29 +3,31 @@ module OwnCloud
   module RecipeHelpers
 
     def generate_certificate
-      cert = OwnCloud::Certificate.new(node['owncloud']['server_name'])
       ssl_key_path = ::File.join(node['owncloud']['ssl_key_dir'], 'owncloud.key')
       ssl_cert_path = ::File.join(node['owncloud']['ssl_cert_dir'], 'owncloud.pem')
 
-      # Create ssl certificate key
-      file 'owncloud.key' do
-        path ssl_key_path
-        owner 'root'
-        group 'root'
-        mode 00600
-        content cert.key
-        action :create_if_missing
-        notifies :create, 'file[owncloud.pem]', :immediately
-      end
+      unless ::File.exists?(ssl_key_path) and ::File.exists?(ssl_cert_path)
+        cert = OwnCloud::Certificate.new(node['owncloud']['server_name'])
 
-      # Create ssl certificate
-      file 'owncloud.pem' do
-        path ssl_cert_path
-        owner 'root'
-        group 'root'
-        mode 00644
-        content cert.cert
-        action :nothing
+        # Create ssl certificate key
+        file 'owncloud.key' do
+          path ssl_key_path
+          owner 'root'
+          group 'root'
+          mode 00600
+          content cert.key
+          action :create
+        end
+
+        # Create ssl certificate
+        file 'owncloud.pem' do
+          path ssl_cert_path
+          owner 'root'
+          group 'root'
+          mode 00644
+          content cert.cert
+          action :create
+        end
       end
 
       [ ssl_key_path, ssl_cert_path ]
