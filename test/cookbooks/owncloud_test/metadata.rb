99c1694722316654d9161c4827eb5f1b8ec54c06
Xabier de Zuazo <xabier@zuazo.org>
2015-09-19 21:11:47 +0200

    Improve `owncloud_test` cookbook description

--- test/cookbooks/owncloud_test/metadata.rb
+++ test/cookbooks/owncloud_test/metadata.rb
@@ -2,7 +2,8 @@ name 'owncloud_test'
 maintainer 'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
 license 'Apache 2.0'
-description 'Installs/Configures owncloud_test'
+description 'This cookbook is used with test-kitchen to test the parent, '\
+            'owncloud cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
