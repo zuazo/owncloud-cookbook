7a439b74518a07b6564e9a6610de558ed3007c5a
Xabier de Zuazo <xabier@zuazo.org>
2015-10-07 09:04:23 +0200

    Improve execute[run owncloud setup] error checking

--- recipes/default.rb
+++ recipes/default.rb
@@ -406,7 +406,7 @@ execute 'run owncloud setup' do
   cwd node['owncloud']['dir']
   command(
     "sudo -u '#{node[web_server]['user']}' php -f index.php "\
-    '| { ! grep -iA2 -e error -e failed; }'
+    '| { ! grep -iA2 -e error -e failed -e "No database drivers"; }'
   )
   action :nothing
 end
